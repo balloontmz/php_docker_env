67dd24717d5d27a040b7f8e4a2b3b186fffcde6c
tomtiddler <tomtiddler@tomtiddlers-iMac-Pro.local>
2019-06-23 22:50:33 +0800

    初始化

---
+++ worker/worker.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# cd /var/www/html/jinhui-mall && php artisan queue:listen --queue=orders,goods,emails --tries=3
+# cd /var/www/html/mes && php artisan queue:listen --queue=emails --tries=1
+
+# tail -f /var/log/dmesg
