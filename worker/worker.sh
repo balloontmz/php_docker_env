51760905cdaf2ce79735f4003fdcf9a21dd12885
zengge <370093982@qq.com>
2019-12-16 14:38:29 +0800

    7.4

--- worker/worker.sh
+++ worker/worker.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-cd /var/www/html/onshopway-api && php artisan queue:listen --queue=gather,goods --tries=3
+cd /var/www/html/jiyun && sudo -u www-data php artisan queue:listen --queue=default --tries=3
 # cd /var/www/html/mes && php artisan queue:listen --queue=emails --tries=1
 
 # tail -f /var/log/dmesg
